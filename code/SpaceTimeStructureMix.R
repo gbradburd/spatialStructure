1f8e83fc376addb5b6605fa05bed0c1cfb554f29
cooplab <gcoop79@gmail.com>
2015-04-13 21:31:43 -0700

    wrote Sherman nugget update

--- code/SpaceTimeStructureMix.R
+++ code/SpaceTimeStructureMix.R
@@ -16,7 +16,8 @@ cluster.admixed.covariance <- function(cluster){
 }
 
 admixed.covariance <- function(cluster.list,n.clusters,shared.mean){
-	Reduce("+",lapply(cluster.list,FUN=cluster.admixed.covariance)) + shared.mean
+	Reduce("+",lapply(cluster.list,FUN=cluster.admixed.covariance)) + shared.mean   
+	
 }
 
 update.admixed.covariance <- function(cluster.list,n.clusters,shared.mean,updated.clusters,admix.prop.matrix.primes){
@@ -309,6 +310,50 @@ sherman_r <- function(Ap, u, v) {
   list(new.inverse = Ap - (Ap %*% u %*% t(v) %*% Ap)/determ.update, determ.update=determ.update)
   
   }
+
+
+update.nugget.i<-function(i,data.list,super.list){ 
+	delta.nugget<-rnorm(1,sd=0.05) 
+	 recover()
+	new.nugget <- delta.nugget + super.list$parameter.list$nuggets[i] 
+
+	new.prior.prob <- prior.prob.nuggets(new.nugget)   ###wanted to do this here, but perhaps not best way
+	if(is.finite(new.prior.prob) ){
+		u <- rep(0,  nrow(super.list$parameter.list$cluster.list[[1]]$covariance ))  ##WILL NEED NUM OF INDS HERE.
+		u[i] <-  delta.nugget
+		v <- rep(0,  nrow(super.list$parameter.list$cluster.list[[1]]$covariance )) ##WILL NEED NUM OF INDS HERE.
+		v[i] <-  1
+		
+		inverse.updated<-sherman_r (super.list$parameter.list$inverse,u,v)   ##YOU DO THE SHERMAN TANK
+		new.determinant <-  super.list$parameter.list$determinant + log(abs(inverse.updated$determ.update)) 
+		new.inverse <- inverse.updated$new.inverse
+
+		if(FALSE){ #TEST matrix inversion
+			new.admixed.covariance<-super.list$parameter.list$admixed.covar
+			new.admixed.covariance[i,i]<-new.admixed.covariance[i,i]+delta.nugget
+			test <- solve(new.admixed.covariance)	 #GID CHECK
+			summary(c(abs(test-new.inverse)))  #GID CHECK
+		}
+		
+		new.likelihood <-   calculate.likelihood.2(data.list, new.inverse ,  new.determinant )[1]
+		old.likelihood <- super.list$mcmc.quantities$likelihood
+		likelihood.ratio <- new.likelihood - old.likelihood
+		
+		prior.ratio <- new.prior.prob - super.list$mcmc.quantities$prior.probs$nuggets[i]
+
+		accept.ratio<-exp(likelihood.ratio +prior.ratio)
+		if( accept.ratio >= runif(1)  ){
+			super.list$mcmc.quantities$prior.probs$nuggets[i]  <- new.prior.prob 
+			super.list$mcmc.quantities$likelihood <- new.likelihood
+			super.list$mcmc.quantities$posterior.prob <- super.list$mcmc.quantities$posterior.prob + likelihood.ratio + prior.ratio
+			super.list$parameter.list$nuggets[i]  <- new.nugget
+			super.list$parameter.list$determinant <- new.determinant	
+			super.list$parameter.list$inverse <- new.inverse 
+		}
+	}
+	return(super.list)
+}
+
 
 
 update.shared.mean<-function(data.list,super.list){ 
