d705c93fa8c5eb0ef7e4780918d185431bf889cc
Gideon Bradburd <gbradburd@ucdavis.edu>
2015-12-06 10:10:23 -0800

    updated haak data

--- datasets/HumanData/make_haak_data_file.R
+++ datasets/HumanData/make_haak_data_file.R
@@ -1,7 +1,10 @@
-setwd("~/Desktop/Dropbox/InspectorSpaceTime/spatialStructure/datasets/HumanData/analyses")
+setwd("~/Dropbox/InspectorSpaceTime/spatialStructure/datasets/HumanData/metadata_prep")
 load("human_sample_covariance.Robj")
+load("human_sample_bin_var.Robj")
 metadata <- read.table("human_sample_metadata.txt",header=TRUE,stringsAsFactors=FALSE)
 haak.dataset <- list("sample.cov" = sample.cov,
 					 "geo.coords" = cbind(metadata$lon,metadata$lat),
-					 "time.coords" = metadata$time)
+					 "time.coords" = metadata$time,
+					 "sample.sizes" = metadata$sample.size,
+					 "bin.var" = bin.var)
 save(haak.dataset,file="haak.dataset.Robj")
